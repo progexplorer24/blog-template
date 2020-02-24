8dd5948da0548425be9361ad2b8cae95a15c84c8
user <progexplorer@gmail.com>
2020-02-24 09:18:13 +0100

    feat(src): clean src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
 import React, { ReactNode } from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import { ReactComponent as Logo } from "../images/logo.svg"
+type HomePageProps = {
+  children?: ReactNode
+}
 
-const IndexPage = (): ReactNode => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Logo />
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const Home: React.FC<HomePageProps> = () => {
+  return <div>HomePage</div>
+}
 
-export default IndexPage
+export default Home
