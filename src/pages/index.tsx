0cdfa2f80df3b573a52da2f191f8f486cb8391f7
user <progexplorer@gmail.com>
2020-03-04 08:20:26 +0100

    feat(pages/index.tsx): display components and add dummy content to the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,17 @@ import { css } from "@emotion/core"
 import Img from "gatsby-image"
 
 import SEO from "react-seo-component"
-import { Layout } from "../components/layout"
+import { Layout } from "../components/layout-elements/layout"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
+import { H1 } from "../components/page-elements/h1"
+import { Dump } from "../components/dump"
+import { theme } from "../utils/global-styles"
+import { H2 } from "../components/page-elements/h2"
+import { H3 } from "../components/page-elements/h3"
+import { H4 } from "../components/page-elements/h4"
+import { P } from "../components/page-elements/p"
+import { Blockquote } from "../components/page-elements/blockquote"
+import { MyLink } from "../components/page-elements/my-link"
 
 type ImageProps = {
   sizes: {
@@ -76,6 +85,47 @@ const Home: React.FC<HomePageProps> = ({ data }) => {
   return (
     <>
       <Layout>
+        <H1>This is first heading</H1>
+        <H2>This is second heading</H2>
+        <H3>This is third heading</H3>
+        <H4>This is fourth heading</H4>
+        <P>
+          Tailwind uses literal color names (like red, green, etc.) and a
+          numeric scale (where 100 is light and 900 is dark) by default. This
+          ends up being fairly practical for most projects, but there are good
+          reasons to use other naming conventions as well.
+        </P>
+        <P>
+          For example, if you&apos;re working on a project that needs to support
+          multiple themes, it might make sense to use more abstract names like
+          primary and secondary:
+        </P>
+        <P strong>
+          Tailwind uses literal color names (like red, green, etc.) and a
+          numeric scale (where 100 is light and 900 is dark) by default. This
+          ends up being fairly practical for most projects, but there are good
+          reasons to use other naming conventions as well.{" "}
+          <MyLink address="/">This is a link</MyLink>
+        </P>
+        <P em>
+          For example, if you&apos;re working on a project that needs to support
+          multiple themes, it might make sense to use more abstract names like
+          primary and secondary: <MyLink address="/">This is a link</MyLink>
+        </P>
+        <Blockquote
+          author="John Wayne, Quoting and citing with <blockquote>"
+          citeSource="http://html5doctor.com/blockquote-q-cite/"
+        >
+          I don&apos;t know why we are here, but I&apos;m pretty sure that it is
+          not in order to enjoy ourselves.
+        </Blockquote>
+        <MyLink address="/">Yet another link</MyLink>
+        <Dump
+          defaultConfig={theme}
+          css={css`
+            ${tw`overflow-x-hidden`}
+          `}
+        />
         <SEO
           title={title}
           description={description}
