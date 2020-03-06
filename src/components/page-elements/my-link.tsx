0f3827c5a34540d8115d42c16b9f660b07934030
user <progexplorer@gmail.com>
2020-03-06 12:02:07 +0100

    refactor(page-elements/my-link): change my-link styling

--- src/components/page-elements/my-link.tsx
+++ src/components/page-elements/my-link.tsx
@@ -2,9 +2,6 @@ import React, { ReactNode } from "react"
 import { Link as GatsbyLink } from "gatsby"
 import tw from "tailwind.macro"
 import { css } from "@emotion/core"
-import { Theme } from "../../utils/tailwind-types"
-import { useTheme } from "emotion-theming"
-import { animateUnderlineOutwards } from "../../utils/animations"
 type MyLinkProps = {
   children?: ReactNode
   external?: boolean
@@ -16,25 +13,18 @@ export const MyLink: React.FC<MyLinkProps> = ({
   external = false,
   address,
 }) => {
-  const theme = useTheme<Theme>()
-  console.log(theme.colors.teal["500"])
-
   const linkStyles = css`
-    ${animateUnderlineOutwards}
-    ${tw`relative inline-block text-teal-500`}
-    &:after {
-      ${tw`bg-teal-500`}
-    }
+    ${tw`relative inline-block text-teal-500 underline hover:text-teal-700`}
   `
 
   return (
     <>
       {external ? (
         <a
-          href="address"
+          href={address}
           target="_blank"
           rel="noopener noreferrer"
-          css={`
+          css={css`
             ${linkStyles}
           `}
         >
