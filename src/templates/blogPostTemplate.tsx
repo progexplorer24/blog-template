ca83fa3231f1082284962a30216d3c6a99077456
user <progexplorer@gmail.com>
2020-03-10 16:06:53 +0100

    feat(templates/blogposttemplate): automatically insert cover images to articles

--- src/templates/blogPostTemplate.tsx
+++ src/templates/blogPostTemplate.tsx
@@ -2,8 +2,11 @@ import { graphql, Link } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import SEO from "react-seo-component"
 import React from "react"
+import { css } from "@emotion/core"
+import tw from "tailwind.macro"
 import { Layout } from "../components/layout-elements/layout"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
+import { Small } from "../components/page-elements"
 
 type BlogPostProps = {
   data: {
@@ -16,9 +19,20 @@ type BlogPostProps = {
       frontmatter: {
         title: string
         date: string
-        cover: {
+        cover?: {
           publicURL: string
+          childImageSharp: {
+            sizes: {
+              tracedSVG: string
+              aspectRatio: number
+              src: string
+              srcSet: string
+              sizes: string
+            }
+          }
         }
+        coverCredit?: string
+        coverAlt?: string
       }
     }
   }
@@ -43,6 +57,31 @@ type BlogPostProps = {
   }
 }
 
+type ImgProps = {
+  source: string
+  alt: string
+  sizes: string
+  credit?: string
+}
+
+const Img: React.FC<ImgProps> = ({ source, alt, credit, sizes }) => (
+  <figure>
+    <img
+      src={source}
+      alt={alt}
+      sizes={sizes}
+      css={css`
+        ${tw`rounded-lg`}
+      `}
+    />
+    {typeof credit === "undefined" ? null : (
+      <figcaption>
+        <Small>{credit}</Small>
+      </figcaption>
+    )}
+  </figure>
+)
+
 const BlogPost: React.FC<BlogPostProps> = ({ data, pageContext }) => {
   const {
     image,
@@ -53,8 +92,10 @@ const BlogPost: React.FC<BlogPostProps> = ({ data, pageContext }) => {
     author,
   } = useSiteMetadata()
   const { frontmatter, body, fields, excerpt } = data.mdx
-  const { title, date, cover } = frontmatter
+  const { title, date, cover, coverCredit, coverAlt } = frontmatter
   const { previous, next } = pageContext
+
+  console.log(frontmatter, coverCredit)
 
   return (
     <Layout>
@@ -62,7 +103,9 @@ const BlogPost: React.FC<BlogPostProps> = ({ data, pageContext }) => {
         title={title}
         description={excerpt}
         image={
-          cover === null ? `${siteUrl}${image}` : `${siteUrl}${cover.publicURL}`
+          typeof cover === "undefined"
+            ? `${siteUrl}${image}`
+            : `${siteUrl}${cover.publicURL}`
         }
         pathname={`${siteUrl}${fields.slug}`}
         siteLanguage={siteLanguage}
@@ -75,7 +118,15 @@ const BlogPost: React.FC<BlogPostProps> = ({ data, pageContext }) => {
       />
       <h1>{title}</h1>
       <p>{date}</p>
-      <MDXRenderer>{body}</MDXRenderer>
+      {typeof cover === "undefined" ? null : (
+        <Img
+          source={cover.publicURL}
+          alt={coverAlt ? coverAlt : "cover image"}
+          sizes={cover.childImageSharp.sizes.sizes}
+          credit={frontmatter.coverCredit}
+        />
+      )}
+      <MDXRenderer frontmatter={frontmatter}>{body}</MDXRenderer>
       {previous === null ? null : (
         <>
           <Link to={previous.fields.slug}>
@@ -105,6 +156,8 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "YYYY MMMM Do")
+        coverCredit
+        coverAlt
         cover {
           publicURL
           childImageSharp {
