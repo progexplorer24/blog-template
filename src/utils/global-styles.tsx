248d583dc82e64b54aef39aaaa76472126e2950a
user <progexplorer@gmail.com>
2020-03-10 09:11:41 +0100

    refactor(utils/global-styles): import projectDefault styles from tailwind.config.js

--- src/utils/global-styles.tsx
+++ src/utils/global-styles.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Global, css } from "@emotion/core"
 import defaultConfig from "tailwindcss/defaultConfig"
+import projectTailwindConfig from "../../tailwind.config"
 import tw from "tailwind.macro"
 
 import base from "tailwindcss/base.css"
@@ -8,35 +9,7 @@ import { Theme } from "./tailwind-types"
 
 export const theme: Theme = {
   ...defaultConfig.theme,
-  fontFamily: {
-    sans: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      "Oxygen",
-      "Ubuntu",
-      "Cantarell",
-      '"Fira sans"',
-      '"Droid Sans"',
-      "Helvetica",
-      "Neue",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-      '"Noto Color Emoji"',
-    ],
-    serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
-    mono: [
-      "Menlo",
-      "Monaco",
-      "Consolas",
-      '"Liberation Mono"',
-      '"Courier New"',
-      "monospace",
-    ],
-  },
+  ...projectTailwindConfig.theme.fontFamily,
 }
 
 type GlobalStylesProps = {}
