34201085a5fe7f2bd06f2ef7219b9ef08fe636bb
user <progexplorer@gmail.com>
2020-03-12 08:03:20 +0100

    feat(page-elements/elements-partials): add linkedHeaders styling

--- src/components/page-elements/elements-partials.tsx
+++ src/components/page-elements/elements-partials.tsx
@@ -2,5 +2,21 @@ import { css } from "@emotion/core"
 import tw from "tailwind.macro"
 
 export const headings = css`
-  ${tw`mt-8 mb-4 font-normal leading-none tracking-tight text-gray-900`}
+  ${tw`flex items-center mt-8 mb-4 font-normal leading-none tracking-tight text-gray-900`}
+`
+
+export const linkedHeaders = css`
+  a {
+    ${tw`pr-1 -ml-5`}
+  }
+  svg {
+    visibility: hidden;
+  }
+  &:hover {
+    a {
+      svg {
+        ${tw`visible w-4 h-4 text-purple-600 fill-current`}
+      }
+    }
+  }
 `
